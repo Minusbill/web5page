6082debe3b85f0b2e65df3bc3c5afc254cbd05ec
bill jian <minus502@126.com>
2023-11-01 13:45:23 +0800

    delete eslint

--- src/components/shared/AppHeaderLinks.vue
+++ src/components/shared/AppHeaderLinks.vue
@@ -16,29 +16,35 @@ export default {
 			aria-label="Projects"
 			>课程</router-link
 		>
-		<router-link
-			to="/hotPoint"
-			class="font-general-medium block text-left text-lg font-medium text-primary-dark dark:text-ternary-light hover:text-indigo-600 dark:hover:text-indigo-300  sm:mx-4 mb-2 sm:py-2 border-t-2 pt-3 sm:pt-2 sm:border-t-0 border-primary-light dark:border-secondary-dark"
-			aria-label="Hot Point"
-			>热点项目</router-link
-		>
+<!--		<router-link-->
+<!--			to="/hotPoint"-->
+<!--			class="font-general-medium block text-left text-lg font-medium text-primary-dark dark:text-ternary-light hover:text-indigo-600 dark:hover:text-indigo-300  sm:mx-4 mb-2 sm:py-2 border-t-2 pt-3 sm:pt-2 sm:border-t-0 border-primary-light dark:border-secondary-dark"-->
+<!--			aria-label="Hot Point"-->
+<!--			>热点教学</router-link-->
+<!--		>-->
 		<router-link
 			to="/newComer"
 			class="font-general-medium block text-left text-lg font-medium text-primary-dark dark:text-ternary-light hover:text-indigo-600 dark:hover:text-indigo-300  sm:mx-4 mb-2 sm:py-2 border-t-2 pt-3 sm:pt-2 sm:border-t-0 border-primary-light dark:border-secondary-dark"
 			aria-label="CewComer"
-			>新人学院</router-link
+			>新人学院👨‍🎓</router-link
 		>
     <router-link
         to="/dawanqu"
         class="font-general-medium block text-left text-lg font-medium text-primary-dark dark:text-ternary-light hover:text-indigo-600 dark:hover:text-indigo-300  sm:mx-4 mb-2 sm:py-2 border-t-2 pt-3 sm:pt-2 sm:border-t-0 border-primary-light dark:border-secondary-dark"
         aria-label="dawanqu"
-    >大湾区专区</router-link
+    >大湾区专区🔥</router-link
     >
     <router-link
         to="/mingwen"
         class="font-general-medium block text-left text-lg font-medium text-primary-dark dark:text-ternary-light hover:text-indigo-600 dark:hover:text-indigo-300  sm:mx-4 mb-2 sm:py-2 border-t-2 pt-3 sm:pt-2 sm:border-t-0 border-primary-light dark:border-secondary-dark"
         aria-label="dawanqu"
     >项目调研</router-link
+    >
+    <router-link
+        to="/aboutUs"
+        class="font-general-medium block text-left text-lg font-medium text-primary-dark dark:text-ternary-light hover:text-indigo-600 dark:hover:text-indigo-300  sm:mx-4 mb-2 sm:py-2 border-t-2 pt-3 sm:pt-2 sm:border-t-0 border-primary-light dark:border-secondary-dark"
+        aria-label="About us"
+    >关于我们</router-link
     >
 		<div
 			class="border-t-2 pt-3 sm:pt-0 sm:border-t-0 border-primary-light dark:border-secondary-dark"
--- src/router/index.js
+++ src/router/index.js
@@ -11,13 +11,13 @@ const routes = [
 		},
 	},
 	{
-		path: '/hotPoint',
-		name: 'hotPoint',
+		path: '/aboutUs',
+		name: 'aboutUs',
 		// route level code-splitting
 		// this generates a separate chunk (about.[hash].js) for this route
 		// which is lazy-loaded when the route is visited.
 		component: () =>
-			import(/* webpackChunkName: "about" */ '../views/HotPoint.vue'),
+			import(/* webpackChunkName: "about" */ '../views/AbotUs.vue'),
 		meta: {
 			title: 'Web5 - About',
 		},
