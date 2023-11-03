bef2519de95be36dac88a09f0a6d86104f442e91
bill jian <minus502@126.com>
2023-11-03 23:40:19 +0800

    update

---
+++ src/views/lesson/Nostr.vue
@@ -0,0 +1,110 @@
+<script setup>
+import { onMounted, onUpdated } from 'vue';
+import feather from 'feather-icons';
+import ProjectHeader from '../../components/projects/ProjectHeader.vue';
+import ProjectInfo from '../../components/web5Component/Web5ProjectInfoNormal.vue';
+import ProjectRelatedProjects from '../../components/projects/ProjectRelatedProjects.vue';
+
+const singleProjectHeader = {
+  singleProjectTitle: 'Taproot Assests 首发的 Nostr',
+  singleProjectDate: 'Nov 1, 2023',
+  singleProjectTag: 'starknet / cairo',
+};
+const projectImages = [
+  {
+    id: 1,
+    title: 'Kabul Project Management UI',
+    img: require('@/assets/images/ui-project-1.jpg'),
+  },
+];
+
+const projectInfo = {
+  clientHeading: '比特币铭文铸造过程',
+  pptUrl:"https://docs.google.com/presentation/d/e/2PACX-1vTnwvmurecuaJ2PEfQsgBftI9G0wMZOx4r_6iKHxHZ4KAMaWfIMz0XDLyBk7O9SJj7Lda-TxeG8NJn4/embed?start=false&loop=false&delayms=3000",
+  companyInfos: [
+    { id: 1, title: '安装钱包', details: '谷歌插件搜索 UniSat Wallet' },
+    { id: 2, title: '铸造', details: ' Unisat 选择 BRC-20 板块' },
+    { id: 3, title: '搜索可铸代币', details: '点击 in-progress' },
+    { id: 4, title: 'gas 选择', details: '确认合适的gas（决定排序，关键）' },
+    { id: 5, title: '付款', details: '钱包点击确定' },
+
+  ],
+  objectivesHeading: '购买与出售',
+  objectivesDetails: ' Unisat 上可以，OKX 钱包也可以',
+  technologies: [
+    {
+      title: '关于 BTC NFT',
+      techs: [' 使用 MagicEden 即可'],
+    },
+  ],
+  projectDetailsHeading: 'Challenge',
+  projectDetails: [
+    {
+      id: 1,
+      details: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nihil vel illum asperiores dignissimos cumque quibusdam et fugiat voluptatem...',
+    },
+    // ... (Continue with the other details)
+  ],
+  socialSharingsHeading: '分享学习课程',
+  socialSharings: [
+    { id: 1, name: 'Twitter', icon: 'twitter', url: 'https://twitter.com/billjian24' },
+    { id: 2, name: 'Instagram', icon: 'instagram', url: 'https://instagram.com/billjian24' },
+    { id: 3, name: 'Facebook', icon: 'facebook', url: 'https://facebook.com/' },
+    { id: 4, name: 'LinkedIn', icon: 'linkedin', url: 'https://linkedin.com/' },
+    { id: 5, name: 'Youtube', icon: 'youtube', url: 'https://www.youtube.com/c/billjian24' },
+  ],
+};
+
+const relatedProject = {
+  relatedProjectsHeading: '最近的项目',
+  relatedProjects: [
+    {
+      id: 1,
+      title: 'Mobile UI',
+      img: require('@/assets/images/starknet.jpg'),
+    },
+    {
+      id: 2,
+      title: 'Web Application',
+      img: require('@/assets/images/raas.jpg'),
+    },
+    {
+      id: 3,
+      title: 'UI Design',
+      img: require('@/assets/images/sui.jpg'),
+    },
+    {
+      id: 4,
+      title: 'Kabul Mobile App UI',
+      img: require('@/assets/images/mev.jpg'),
+    },
+  ],
+};
+
+
+onMounted(() => {
+  feather.replace();
+});
+
+onUpdated(() => {
+  feather.replace();
+});
+</script>
+
+<template>
+  <div class="container mx-auto mt-10 sm:mt-20">
+    <!-- Project header -->
+    <ProjectHeader :singleProjectHeader="singleProjectHeader" />
+
+    <!-- Project gallery -->
+    <!--    <ProjectGallery :projectImages="projectImages" />-->
+
+    <!-- Project information -->
+    <ProjectInfo :projectInfo="projectInfo" />
+
+    <!-- Project related projects -->
+    <ProjectRelatedProjects :relatedProject="relatedProject" />
+  </div>
+</template>
+
+<style scoped></style>
