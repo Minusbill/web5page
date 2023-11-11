3d0b1b1985dfbdcfc6bd140b7c879b9672b3dc8d
bill jian <minus502@126.com>
2023-11-11 10:31:22 +0800

    update

--- src/components/shared/InscFlowMenu.vue
+++ src/components/shared/InscFlowMenu.vue
@@ -11,7 +11,7 @@ const isVisible = ref(false);
   <div class="inset-0" @click="isVisible = false"></div>
   <div class="relative inline-block" @mouseover="isVisible = true" @mouseleave="isVisible = false" @keydown.enter="isVisible = !isVisible">
     <button type="button" class="inline-flex items-center justify-between px-2 py-1 font-medium text-gray-700 transition-all duration-500 rounded-md focus:outline-none focus:text-brand-900 sm:focus:shadow-outline">
-      <span class="font-general-medium block text-left text-lg font-medium text-primary-dark dark:text-ternary-light hover:text-indigo-600 dark:hover:text-indigo-300  sm:mx-4 mb-2 sm:py-2 ">比特生态/铭文专区🔥</span>
+      <span class="font-general-medium block text-left text-lg font-medium text-primary-dark dark:text-ternary-light hover:text-indigo-600 dark:hover:text-indigo-300  sm:mx-4 mb-2 sm:py-2 ">比特生态/铭文🔥</span>
       <svg fill="currentColor" viewBox="0 0 20 20" class="flex-shrink-0 w-5 h-5 ml-1">
         <path :class="{ 'rotate-180': isVisible }" class="transition duration-300 ease-in-out origin-center transform" fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd"></path>
       </svg>
@@ -31,7 +31,7 @@ const isVisible = ref(false);
                 to="/insc/btc"
                 class="block w-full px-20 py-2 font-medium text-gray-700 whitespace-no-wrap hover:bg-gray-100 focus:outline-none hover:text-gray-900 focus:text-gray-900 focus:shadow-outline transition duration-300 ease-in-out"
                 aria-label="About us"
-            >比特币</router-link
+            >比特币生态</router-link
             >
           </div>
         </div>
