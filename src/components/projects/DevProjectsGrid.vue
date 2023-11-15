f3231587a0f78e2e89a49aa446cf349fa97575a1
bill jian <minus502@126.com>
2023-11-16 01:25:28 +0800

    update

--- src/components/projects/DevProjectsGrid.vue
+++ src/components/projects/DevProjectsGrid.vue
@@ -1,138 +1,90 @@
-<script setup>
-import { ref, computed, onMounted } from 'vue';
-import feather from 'feather-icons';
-import ProjectSingle from './DwqProjectSingle.vue';
-import projectsData from '../../data/devProjects';
-
-// Data
-const projects = ref(projectsData);
-const projectsHeading = ref('开发者接单平台');
-const selectedCategory = ref('');
-const searchProject = ref('');
-
-// Computed
-const filteredProjects = computed(() => {
-  if (selectedCategory.value) {
-    return filterProjectsByCategory();
-  } else if (searchProject.value) {
-    return filterProjectsBySearch();
-  }
-  return projects.value;
-});
-
-// Methods
-function filterProjectsByCategory() {
-  return projects.value.filter((item) => {
-    let category = item.category.charAt(0).toUpperCase() + item.category.slice(1);
-    return category.includes(selectedCategory.value);
-  });
-}
-
-function filterProjectsBySearch() {
-  let project = new RegExp(searchProject.value, 'i');
-  return projects.value.filter((el) => el.title.match(project));
-}
-
-onMounted(() => {
-  feather.replace();
-});
-</script>
-
-
 <template>
-	<!-- Projects grid -->
-	<section class="pt-10 sm:pt-14">
-		<!-- Projects grid title -->
-		<div class="text-center">
-			<p
-				class="font-general-semibold text-2xl sm:text-5xl font-semibold mb-2 text-ternary-dark dark:text-ternary-light"
-			>
-				{{ projectsHeading }}
-			</p>
-		</div>
+  <div class="text-center">
+    <p
+        class="font-general-semibold text-2xl sm:text-5xl font-semibold mb-2 text-ternary-dark dark:text-ternary-light"
+    >
+      接单平台
+    </p>
+  </div>
 
-		<!-- Filter and search projects -->
-		<div class="mt-10 sm:mt-10">
-			<h3
-				class="font-general-regular
-					text-center text-secondary-dark
-					dark:text-ternary-light
-					text-md
-					sm:text-xl
-					font-normal
-					mb-4
-				"
-			>
-<!--				铭文入门学习、监控 以及工具使用、赚钱-->
-			</h3>
-<!--			<div-->
-<!--				class="-->
-<!--					flex-->
-<!--					justify-between-->
-<!--					border-b border-primary-light-->
-<!--					dark:border-secondary-dark-->
-<!--					pb-3-->
-<!--					gap-2-->
-<!--				"-->
-<!--			>-->
-<!--				<div class="flex justify-between gap-2">-->
-<!--					<span-->
-<!--						class="-->
-<!--							hidden-->
-<!--							sm:block-->
-<!--							bg-primary-light-->
-<!--							dark:bg-ternary-dark-->
-<!--							p-2.5-->
-<!--							shadow-sm-->
-<!--							rounded-xl-->
-<!--							cursor-pointer-->
-<!--							"-->
-<!--					>-->
-<!--						<i-->
-<!--							data-feather="search"-->
-<!--							class="text-ternary-dark dark:text-ternary-light"-->
-<!--						></i>-->
-<!--					</span>-->
-<!--					<input-->
-<!--						v-model="searchProject"-->
-<!--						class="font-general-medium-->
-<!--						pl-3-->
-<!--						pr-1-->
-<!--						sm:px-4-->
-<!--						py-2-->
-<!--						border-1 border-gray-200-->
-<!--						dark:border-secondary-dark-->
-<!--						rounded-lg-->
-<!--						text-sm-->
-<!--						sm:text-md-->
-<!--						bg-secondary-light-->
-<!--						dark:bg-ternary-dark-->
-<!--						text-primary-dark-->
-<!--						dark:text-ternary-light-->
-<!--						"-->
-<!--						id="name"-->
-<!--						name="name"-->
-<!--						type="search"-->
-<!--						required=""-->
-<!--						placeholder="Search Projects"-->
-<!--						aria-label="Name"-->
-<!--					/>-->
-<!--				</div>-->
-<!--				<ProjectsFilter @filter="selectedCategory = $event" />-->
-<!--			</div>-->
-		</div>
-
-		<!-- Projects grid -->
-		<div
-			class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 mt-6 sm:gap-10"
-		>
-			<ProjectSingle
-				v-for="project in filteredProjects"
-				:key="project.id"
-				:project="project"
-			/>
-		</div>
-	</section>
+  <ul role="list" class="divide-y divide-gray-100">
+    <li v-for="person in people" :key="person.email" class="flex justify-between gap-x-6 py-5">
+      <div class="flex min-w-0 gap-x-4">
+        <img class="h-12 w-12 flex-none rounded-full bg-gray-50" :src="person.imageUrl" alt="" />
+        <div class="min-w-0 flex-auto">
+          <p class="text-sm font-semibold leading-6 text-gray-900">{{ person.name }}</p>
+          <p class="mt-1 truncate text-xs leading-5 text-gray-500">{{ person.email }}</p>
+        </div>
+      </div>
+      <div class="hidden shrink-0 sm:flex sm:flex-col sm:items-end">
+        <p class="text-sm leading-6 text-gray-900">{{ person.role }}</p>
+        <p v-if="person.lastSeen" class="mt-1 text-xs leading-5 text-gray-500">
+          Last seen <time :datetime="person.lastSeenDateTime">{{ person.lastSeen }}</time>
+        </p>
+        <div v-else class="mt-1 flex items-center gap-x-1.5">
+          <div class="flex-none rounded-full bg-emerald-500/20 p-1">
+            <div class="h-1.5 w-1.5 rounded-full bg-emerald-500" />
+          </div>
+          <p class="text-xs leading-5 text-gray-500">Online</p>
+        </div>
+      </div>
+    </li>
+  </ul>
 </template>
 
-<style scoped></style>
+<script setup>
+const people = [
+  {
+    name: 'Leslie Alexander',
+    email: 'leslie.alexander@example.com',
+    role: 'Co-Founder / CEO',
+    imageUrl:
+        'https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+    lastSeen: '3h ago',
+    lastSeenDateTime: '2023-01-23T13:23Z',
+  },
+  {
+    name: 'Michael Foster',
+    email: 'michael.foster@example.com',
+    role: 'Co-Founder / CTO',
+    imageUrl:
+        'https://images.unsplash.com/photo-1519244703995-f4e0f30006d5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+    lastSeen: '3h ago',
+    lastSeenDateTime: '2023-01-23T13:23Z',
+  },
+  {
+    name: 'Dries Vincent',
+    email: 'dries.vincent@example.com',
+    role: 'Business Relations',
+    imageUrl:
+        'https://images.unsplash.com/photo-1506794778202-cad84cf45f1d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+    lastSeen: null,
+  },
+  {
+    name: 'Lindsay Walton',
+    email: 'lindsay.walton@example.com',
+    role: 'Front-end Developer',
+    imageUrl:
+        'https://images.unsplash.com/photo-1517841905240-472988babdf9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+    lastSeen: '3h ago',
+    lastSeenDateTime: '2023-01-23T13:23Z',
+  },
+  {
+    name: 'Courtney Henry',
+    email: 'courtney.henry@example.com',
+    role: 'Designer',
+    imageUrl:
+        'https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+    lastSeen: '3h ago',
+    lastSeenDateTime: '2023-01-23T13:23Z',
+  },
+  {
+    name: 'Tom Cook',
+    email: 'tom.cook@example.com',
+    role: 'Director of Product',
+    imageUrl:
+        'https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+    lastSeen: null,
+  },
+]
+</script>
