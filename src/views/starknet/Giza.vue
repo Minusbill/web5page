51ec4e55fa5decd7882b9fd3d083f3abad3d6062
bill jian <minus502@126.com>
2023-12-01 22:54:34 +0800

    update

--- src/views/starknet/Giza.vue
+++ src/views/starknet/Giza.vue
@@ -22,10 +22,13 @@ const projectInfo = {
   clientHeading: 'Giza 与 Giza CLI',
   pptUrl:"https://docs.google.com/presentation/d/e/2PACX-1vQk51feZGL0B4kT4zoyZKFSBxQsb9-8pTxn6lA6k6WrTEFUl93QFQNy7veZ8SDrKsUKLLet6-HkDowe/embed?start=false&loop=false&delayms=3000",
   companyInfos: [
+    { id: 1, title: 'Giza ', details: 'Giza 是一个基于 StarkNet 构建的机器学习平台，StarkNet 是以太坊的无需许可的去中心化 ZK-Rollup。 它旨在通过利用汇总解决方案和零知识证明来解决在 web2 解决方案中部署和扩展 ML 模型的挑战。\n' +
+          '\n' },
+    { id: 2, title: 'Giza CLI', details: 'CLI 作为Giza生态系统的主要入口点，简化了资源管理以及与吉萨堆栈的交互。在可验证机器学习这样不断发展的领域中，技术可能很复杂，但作为开发人员与其交互应该是直观的。' },
 
   ],
-  objectivesHeading: '关于Giza ',
-  objectivesDetails: 'Giza 正在构建一个无需信任的协议，将机器学习推理计算的过程去中心化，同时为开源人工智能的开放经济提供动力。Giza 让人工智能开发者可以轻松地为人工智能模型生成零知识证明。',
+  objectivesHeading: ' ',
+  objectivesDetails: '',
   technologies: [
     {
       title: '',
